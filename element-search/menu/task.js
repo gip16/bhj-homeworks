3301ee5a5750298b88b510f6f4a5fd15900a8c3c
“Aleksandr <gipers16@gmail.com>
2023-02-15 21:23:54 +0300

    Exercise 1.2 fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,9 +1,11 @@
 const links = Array.from(document.querySelectorAll('.menu__link'));
+const subMenu = Array.from(document.querySelectorAll('.menu_sub'));
 
 links.forEach(link => {
   link.onclick = () => {
-    if (link.closest('.menu_sub') !== null) {
-      link.closest('.menu_sub').classList.add('menu_active');
+    if (link.parentElement.querySelector('.menu_sub') !== null) {
+      subMenu.forEach(item => item.classList.remove('menu_active'));
+      link.parentElement.querySelector('.menu_sub').classList.add('menu_active');
       return false;
     }
   };
