a2ae405dd1d61c726a477f444f23d4eb338ccb45
“Aleksandr <gipers16@gmail.com>
2023-03-09 22:19:06 +0300

    Exercise 3.2 fix

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -3,7 +3,17 @@ const close = document.querySelector('.modal__close');
 
 close.addEventListener('click', () => modal.classList.remove('modal_active'));
 
-if (!document.cookie) {
+function getCookie() {
+  return document.cookie.split('; ').reduce((acc, item) => {
+    const [name, value] = item.split('=');
+    acc[name] = value;
+    return acc;
+  }, {});
+}
+
+const cookie = getCookie();
+
+if (!cookie.modal) {
   modal.classList.add('modal_active');
   document.cookie = 'modal=true';
 }
